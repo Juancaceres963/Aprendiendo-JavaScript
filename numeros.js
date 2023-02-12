93076e28f499b7c259e7409679cbe3ce15406e01
Juancaceres963 <dnijuancaceres@gmail.com>
2023-02-12 18:12:27 -0300

    Transformacion de Datos

--- numeros.js
+++ numeros.js
@@ -1,4 +1,51 @@
+console.log("Seccion de funciones y metodos para numeros");
+
+// Funciones y metodos para numeros 
+let numero1 = 50,
+    numero2 = "10",
+    numero3 = "tres",
+    numero4 = "20.20"
+
+// Convertir un string a numero 
+console.log(Number(numero2) + numero1);
+
+// ParseInt se encargar de converit un string a numero, y a demas de que hace que el numero sea entero
+console.log(parseInt(numero4) + numero1);
+
+// ParseFloat se encargar de converit un string a numero, y de dejar los desimales si el numero asi lo tiene
+console.log(parseFloat(numero4) + numero1);
+
+// Otros datos en convercion
+console.log("Seccion de Otros datos en convercion");
+
+let dato;
+//Convertir de boolean a numero 
+dato = Number(true);
+console.log(dato);
+
+dato = Number(false);
+console.log(dato);
+
+//otras conversiones con number
+dato = Number([1,2,3,4]);
+console.log(dato);
+
+dato = Number(null);
+console.log(dato);
+
+dato = Number("hola mundo");
+console.log(dato);
+
+// To Fixed elimina las desimales pero devuelve en string
+// To fixed solo puede ser aplicado en numeros
+// Puede aceptar o no un parametro, este sera en numero y define si se quiere dejar alguna cantidad de decimales
+dato = 1243546.435345677;
+console.log(dato.toFixed(4));
+dato = Number("1243546.435345677");
+console.log(dato.toFixed());
+
 // Math 
+console.log("Seccion de Math ");
 // Round esta se va a encargar de redondiar un numero
 
 resultado = Math.round(2.5)
@@ -49,3 +96,22 @@ carrito -= 40;
 console.log(carrito)
 
 //En este ejemplo de carrito fue sumando a la variable cada ves que estaba el +=, y se resta cada vez que -=
+
+// Concerciones de numero a string
+console.log("Convertir de numero a string");
+
+// El metodo String en este caso es el qu ehace posible qu ese pueda saber la longitud del numero, ya que .length no funciona con numeros 
+
+let id = 19435346578;
+    id = String(id);
+
+console.log(id);
+console.log(id.length);
+
+// Deto de Booleano a string
+
+let boolean = true;
+    boolean = String(boolean);
+
+console.log(boolean);
+console.log(boolean.length);

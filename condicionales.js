12f3de4c8926b3f8a9d7b1a313f154c58c391e18
Juancaceres963 <dnijuancaceres@gmail.com>
2023-02-14 10:02:30 -0300

    Operador Ternario

--- condicionales.js
+++ condicionales.js
@@ -27,3 +27,22 @@ if (dinero > carrito){
 } else {
     console.log("Fondos insuficientes");
 }
+
+// Operadores Booleanos Logicos || y &&
+
+let hora = 18;
+if (hora > 0 && hora <= 12) {
+    console.log("Buenos dias");
+} else if (hora > 12 && hora <= 18) {
+    console.log("Buenas tardes");
+} else if (hora > 18 && hora <= 24) {
+    console.log("Buenas noches")
+} else {
+    console.log("Horario no valido")
+}
+
+// Operador Ternario (condicional)
+console.log("Seccion de operador ternario")
+
+let logueado = false;
+console.log( logueado ? "Estas logueado" : "Iniciar secion");
